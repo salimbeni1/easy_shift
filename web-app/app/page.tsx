0f6bb88c44ca6cc6ad4f3d7febc3686e60956991
salimbeni1 <etienne.salimbeni@epfl.ch>
2024-01-18 18:06:15 +0100

    dates

--- web-app/app/page.tsx
+++ web-app/app/page.tsx
@@ -1,21 +1,40 @@
 import { SchedulerGrid } from '@/components/SchedulerGrid'
 import Image from 'next/image'
 
+import { LuSend } from "react-icons/lu";
+import { PiFilePdfDuotone } from "react-icons/pi";
+
 export default function Home() {
   return (
     
 
       <main className= "m-10 w-100vw h-100vh overflow-hidden ">
       
-          <div>
+          <div className='flex justify-between'>
 
-              <h1 className= "text-green-600 font-bold text-2xl pb-6"> Easy Shift </h1>
+              <h1 className= "text-green-600 font-bold text-3xl pb-6"> Easy Shift </h1>
+
+              <div className= "font-bold text-2xl pb-6 cursor-pointer" >
+                login
+              </div>
 
           </div>
 
           <div>
 
             <SchedulerGrid/>
+
+          </div>
+
+          <div className='flex pt-6 justify-end gap-3 pr-3'>
+            
+            <div>
+              <PiFilePdfDuotone className='fill-green-600 w-14 h-14 cursor-pointer'/>
+            </div>
+
+            <div>
+              <LuSend className='stroke-green-600 w-12 h-12 cursor-pointer'/>
+            </div>
 
           </div>
       
--- web-app/components/SchedulerGrid/index.tsx
+++ web-app/components/SchedulerGrid/index.tsx
@@ -2,11 +2,80 @@
 "use client"
 
 import React, { useRef, useState, useEffect } from 'react';
+import { IoSearch } from "react-icons/io5";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa6";
+import { PiUserPlusBold } from "react-icons/pi";
+
+let users = [
+    {
+        name:"Etienne",
+        image:"profiles/2.png",
+        slots : [
+            { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0), details:"" ,type:"1" },
+            { start: new Date(2024, 0, 1, 13, 0), end: new Date(2024, 0, 2, 15, 0), details:"" ,type:"1" },
+        ]
+    },
+    {
+        name:"Milos",
+        image:"profiles/1.png",
+        slots : [
+            { start: new Date(2024, 0, 17, 4, 0), end: new Date(2024, 0, 17, 15, 0), details:"" ,type:"2" },
+            { start: new Date(2024, 0, 18, 13, 0), end: new Date(2024, 0, 19, 15, 0), details:"" ,type:"1" },
+        ]
+    }
+    ,
+    {
+        name:"Renata",
+        image:"profiles/3.png",
+        slots : [
+            { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0), details:"",type:"3"  },
+            { start: new Date(2024, 0, 12, 13, 0), end: new Date(2024, 0, 13, 10, 0), details:"",type:"3" },
+        ]
+    }
+    ,
+    {
+        name:"Paul",
+        image:"profiles/4.png",
+        slots : [
+            { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0), details:"" ,type:"1" },
+            { start: new Date(2024, 0, 12, 13, 0), end: new Date(2024, 0, 13, 10, 0), details:"" ,type:"2" },
+        ]
+    }
+    ,
+    {
+        name:"Eliot",
+        image:"profiles/5.png",
+        slots : [
+            { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0), details:"" ,type:"2" },
+            { start: new Date(2024, 0, 12, 13, 0), end: new Date(2024, 0, 13, 10, 0), details:"" ,type:"1" },
+        ]
+    }
+    ,
+    {
+        name:"Sara",
+        image:"profiles/6.png",
+        slots : [
+            { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0), details:"" ,type:"1" },
+            { start: new Date(2024, 0, 12, 13, 0), end: new Date(2024, 0, 13, 10, 0), details:"" ,type:"3" },
+        ]
+    },
+    {
+        name:"Ayoub",
+        image:"profiles/7.png",
+        slots : [
+            { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0), details:"" ,type:"2"},
+            { start: new Date(2024, 0, 12, 13, 0), end: new Date(2024, 0, 13, 10, 0), details:""  ,type:"2"},
+        ]
+    }
+]
+
 
 export function SchedulerGrid () {
 
     const timetableRef = useRef<HTMLDivElement>(null);
     const [timetableWidth, setTimetableWidth] = useState(0);
+    const [selectedWeek, setSelectedWeek] = useState<Date>(new Date())
+    const [searchName, setSearchName] = useState<string>("")
 
     const updateWidth = () => {
         if (timetableRef.current) {
@@ -21,76 +90,34 @@ export function SchedulerGrid () {
     }, []); 
 
 
-    let users = [
-        {
-            name:"Etienne",
-            image:"profiles/2.png",
-            slots : [
-                { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0) },
-                { start: new Date(2024, 0, 1, 13, 0), end: new Date(2024, 0, 2, 15, 0) },
-            ]
-        },
-        {
-            name:"Milos",
-            image:"profiles/1.png",
-            slots : [
-                { start: new Date(2024, 0, 17, 4, 0), end: new Date(2024, 0, 17, 15, 0) },
-                { start: new Date(2024, 0, 18, 13, 0), end: new Date(2024, 0, 19, 15, 0) },
-            ]
+    const get_users = () => {
+        if (searchName == ""){
+            return users
         }
-        ,
-        {
-            name:"Renata",
-            image:"profiles/3.png",
-            slots : [
-                { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0) },
-                { start: new Date(2024, 0, 12, 13, 0), end: new Date(2024, 0, 13, 10, 0) },
-            ]
-        }
-        ,
-        {
-            name:"Paul",
-            image:"profiles/4.png",
-            slots : [
-                { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0) },
-                { start: new Date(2024, 0, 12, 13, 0), end: new Date(2024, 0, 13, 10, 0) },
-            ]
-        }
-        ,
-        {
-            name:"Eliot",
-            image:"profiles/5.png",
-            slots : [
-                { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0) },
-                { start: new Date(2024, 0, 12, 13, 0), end: new Date(2024, 0, 13, 10, 0) },
-            ]
-        }
-        ,
-        {
-            name:"Sara",
-            image:"profiles/6.png",
-            slots : [
-                { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0) },
-                { start: new Date(2024, 0, 12, 13, 0), end: new Date(2024, 0, 13, 10, 0) },
-            ]
-        },
-        {
-            name:"Ayoub",
-            image:"profiles/7.png",
-            slots : [
-                { start: new Date(2024, 0, 17, 9, 0), end: new Date(2024, 0, 17, 11, 0) },
-                { start: new Date(2024, 0, 12, 13, 0), end: new Date(2024, 0, 13, 10, 0) },
-            ]
-        }
-    ]
+        return users.filter( e => e.name.toLocaleLowerCase().includes(searchName.toLocaleLowerCase()))
+    }
 
+    function weekDetailsToString( details:any ){
+        return "" + details.firstDayOfWeek.getDate() + "-" + details.lastDayOfWeek.getDate() + " / " + details.month + " / " + details.year
+    }
 
     const totalMinutesInWeek = 7 * 24 * 60;
 
     const getMinutesFromWeekStart = (date: Date) => {
         const startOfWeek = new Date(date.getFullYear(), date.getMonth(), date.getDate() - date.getDay(), 0, 0, 0);
-        return (date.getTime() - startOfWeek.getTime()) / 60000; // Convert milliseconds to minutes
+        return (date.getTime() - startOfWeek.getTime()) / 60000;
     };
+
+    function getWeekDetails(inputDate: Date): { firstDayOfWeek: Date, lastDayOfWeek: Date, month: number, year: number } {
+        let date = new Date(inputDate.getTime());
+        const dayOfWeek = date.getDay();
+        const firstDayOfWeek = new Date(date.setDate(date.getDate() - dayOfWeek));
+        date = new Date(inputDate.getTime()); 
+        const lastDayOfWeek = new Date(date.setDate(date.getDate() - dayOfWeek + 6));
+        const month = inputDate.getMonth() + 1;
+        const year = inputDate.getFullYear();
+        return { firstDayOfWeek, lastDayOfWeek, month, year };
+    }
 
     const calculateSlotPositionAndWidth = (start: Date, end: Date) => {
         const startMinutes = getMinutesFromWeekStart(start);
@@ -102,7 +129,6 @@ export function SchedulerGrid () {
     
         return { left, width };
     };
-
 
 
     const renderSlots = (slots: any[]) => {
@@ -121,10 +147,57 @@ export function SchedulerGrid () {
     };
 
     return (
-        <div className="border" >
+
+        <>
+        <div className="border overflow-hidden" >
+            <div className="flex flex-row items-center">
+                <div  className="
+                flex flex-row items-center gap-1 w-48 p-2 border-r overflow-hidden top-full h-12
+                "> </div>
+                <div
+                ref={timetableRef}
+                className="border-left w-full relative top-0
+                flex flex-row justify-around
+                ">
+                    <div> <FaAngleLeft className="w-8 h-8 fill-green-600 cursor-pointer"/> </div>
+                    <div> { weekDetailsToString(getWeekDetails(selectedWeek)) } </div> 
+                    <div> <FaAngleRight className="w-8 h-8 fill-green-600 cursor-pointer"/> </div>                     
+                </div>
+            </div>
+
+            <div className="flex flex-row items-center">
+                <div  className="
+                flex flex-row items-center gap-1 w-48 p-2 border-r overflow-hidden top-full
+                "> 
+                    <div className="w-12 h-12 rounded-full overflow-hidden">
+                        <IoSearch className="w-10 h-10 fill-green-600" />
+                    </div>
+                    <div> 
+                        <input 
+                        className='w-28 border-b-green-600 border-b-2 focus:outline-none' 
+                        placeholder="Chercher.."
+                        onChange={e => {setSearchName(e.target.value)}} value={searchName}/>    
+                    </div>
+                </div>
+                <div ref={timetableRef}
+                className="border-left w-full relative top-0 grid grid-cols-7">
+                    {
+                        ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"].map(
+                            e => {
+                                return <>
+                                    <div className='relative flex justify-center'>
+                                        <div> {e} </div>
+                                        <div className='absolute border-r-2 right-0 top-0' style={{height:1000}}></div> 
+                                    </div> 
+                                </>
+                            }
+                        )
+                    }              
+                </div>
+            </div>
 
             {
-                users.map( 
+                get_users().map( 
                     u => { 
                         return <div key={u.name+"n"} 
                             className=" hover:bg-green-100 flex flex-row items-center cursor-pointer"
@@ -133,12 +206,10 @@ export function SchedulerGrid () {
                             flex flex-row items-center gap-4 w-48 p-2 border-r overflow-hidden top-full
                             hover:bg-green-100
                             "> 
-                                
                                 <div className="w-12 h-12 rounded-full overflow-hidden">
                                     <img src={u.image} alt=""/>
                                 </div>
                                 <div> {u.name} </div>
-                            
                             </div>
 
                             <div
@@ -151,6 +222,26 @@ export function SchedulerGrid () {
                         </div> } 
                 )
             }
+            <div className="flex flex-row items-center">
+                <div  className="
+                flex flex-row items-center gap-4 w-48 p-2 border-r overflow-hidden top-full cursor-pointer
+                "> 
+                    <div className="pl-2 w-12 h-12 rounded-full overflow-hidden">
+                        <PiUserPlusBold className="w-10 h-10 fill-green-600" />
+                    </div>
+                    <div> ajouter</div>
+                
+                </div>
+
+                <div
+                ref={timetableRef}
+                className="border-left w-full relative top-0
+                flex flex-row justify-around
+                ">
+                                         
+                </div>
+            </div>
         </div>
+        </>
     );
 }
